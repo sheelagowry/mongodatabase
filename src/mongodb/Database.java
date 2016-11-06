72bfc01d72fa91ec74f213062f8ca3d301846380
Sheela Gowry <sheelagowry@gmail.com>
2016-11-06 11:27:53 +0200

    Format

--- src/mongodb/Database.java
+++ src/mongodb/Database.java
@@ -1,97 +1,104 @@
 package mongodb;
+
 import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.Random;
 
 public class Database {
 	public static void main(String[] args) {
-		try{
-		    PrintWriter writer = new PrintWriter("output1.txt", "UTF-8");
-		  //  writer.println("The first line");
-		//    writer.println("The second line");
-		   
-		
-   	 String name;
-   	 // BEFORE 1980
-   	 for(int i = 1;i<=100;i++){
-   		    name = "\"Tom\"";
-   		    
-   		 Random random = new Random();
-   		int minDay = (int) LocalDate.of(1940, 1, 1).toEpochDay();
-   		int maxDay = (int) LocalDate.of(1980, 1, 1).toEpochDay();
-   		long randomDay = minDay + random.nextInt(maxDay - minDay);
+		try {
+			PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
 
-   		LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+			String name;
+			// BEFORE 1980
+			for (int i = 1; i <= 100; i++) {
+				name = "\"Tom\"";
 
-   	
-   		//System.out.printf("db.clients.insert({name:%s,birthdate:"+"\"%s"+"\"});%n",name  , randomBirthDate );
-   		writer.printf("db.clients.insert({name:%s,birthdate:"+"\"%s"+"\"});%n",name  , randomBirthDate );
+				Random random = new Random();
+				int minDay = (int) LocalDate.of(1940, 1, 1).toEpochDay();
+				int maxDay = (int) LocalDate.of(1980, 1, 1).toEpochDay();
+				long randomDay = minDay + random.nextInt(maxDay - minDay);
 
-    }
-   	 
-   	for(int i = 1;i<=3900;i++){
-	    String names[] = {"Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo", "Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur", "Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro", "Bradley"};
-	    Random random = new Random();
-   		int minDay = (int) LocalDate.of(1940, 1, 1).toEpochDay();
-   		int maxDay = (int) LocalDate.of(1980, 1, 1).toEpochDay();
-   		long randomDay = minDay + random.nextInt(maxDay - minDay);
+				LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
 
-   		LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
-       // System.out.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n",names[new Random().nextInt(names.length)], randomBirthDate );
-   		writer.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n",names[new Random().nextInt(names.length)], randomBirthDate );
-	     }
-   	
-   	//AFTER 1990
-   	for(int i = 1;i<=100;i++){
-		    name = "Tom";
-		    String names[] = {"Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo", "Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur", "Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro", "Bradley"};
-	        
-		    Random random = new Random();
-	   		int minDay = (int) LocalDate.of(1990, 1, 1).toEpochDay();
-	   		int maxDay = (int) LocalDate.of(2017, 1, 1).toEpochDay();
-	   		long randomDay = minDay + random.nextInt(maxDay - minDay);
+				writer.printf("db.clients.insert({name:%s,birthdate:" + "\"%s" + "\"});%n", name, randomBirthDate);
 
-	   		LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
-	        //System.out.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n", name + " " + names[new Random().nextInt(names.length)], randomBirthDate );
-	   		writer.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n", name + " " + names[new Random().nextInt(names.length)], randomBirthDate );
+			}
 
-   	}
-   	for(int i = 1;i<=3900;i++){
-	    String names[] = {"Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo", "Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur", "Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro", "Bradley"};
-        
-        Random random = new Random();
-   		int minDay = (int) LocalDate.of(1990, 1, 1).toEpochDay();
-   		int maxDay = (int) LocalDate.of(2017, 1, 1).toEpochDay();
-   		long randomDay = minDay + random.nextInt(maxDay - minDay);
+			for (int i = 1; i <= 3900; i++) {
+				String names[] = { "Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo",
+						"Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur",
+						"Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro",
+						"Bradley" };
+				Random random = new Random();
+				int minDay = (int) LocalDate.of(1940, 1, 1).toEpochDay();
+				int maxDay = (int) LocalDate.of(1980, 1, 1).toEpochDay();
+				long randomDay = minDay + random.nextInt(maxDay - minDay);
 
-   		LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
-		
-      //  System.out.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n",names[new Random().nextInt(names.length)], randomBirthDate );
-   	      writer.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n",names[new Random().nextInt(names.length)], randomBirthDate );
-	 
-	     }
-   	
-   	// BETWEEN 1980 - 1990
-   	for(int i = 1;i<=2000;i++){
-	    String names[] = {"Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo", "Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur", "Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro", "Bradley"};
-	    Random random = new Random();
-   		int minDay = (int) LocalDate.of(1981, 1, 1).toEpochDay();
-   		int maxDay = (int) LocalDate.of(1990, 1, 1).toEpochDay();
-   		long randomDay = minDay + random.nextInt(maxDay - minDay);
+				LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
 
-   		LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
-       // System.out.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n",names[new Random().nextInt(names.length)], randomBirthDate );
-        writer.printf("db.clients.insert({name:"+"\"%s"+"\",birthdate:"+"\"%s"+"\"});%n",names[new Random().nextInt(names.length)], randomBirthDate );
-	
-	     }
-    writer.close();
-   	 
+				writer.printf("db.clients.insert({name:" + "\"%s" + "\",birthdate:" + "\"%s" + "\"});%n",
+						names[new Random().nextInt(names.length)], randomBirthDate);
+			}
+
+			// AFTER 1990
+			for (int i = 1; i <= 100; i++) {
+				name = "Tom";
+				String names[] = { "Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo",
+						"Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur",
+						"Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro",
+						"Bradley" };
+
+				Random random = new Random();
+				int minDay = (int) LocalDate.of(1990, 1, 1).toEpochDay();
+				int maxDay = (int) LocalDate.of(2017, 1, 1).toEpochDay();
+				long randomDay = minDay + random.nextInt(maxDay - minDay);
+
+				LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+
+				writer.printf("db.clients.insert({name:" + "\"%s" + "\",birthdate:" + "\"%s" + "\"});%n",
+						name + " " + names[new Random().nextInt(names.length)], randomBirthDate);
+
+			}
+			for (int i = 1; i <= 3900; i++) {
+				String names[] = { "Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo",
+						"Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur",
+						"Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro",
+						"Bradley" };
+
+				Random random = new Random();
+				int minDay = (int) LocalDate.of(1990, 1, 1).toEpochDay();
+				int maxDay = (int) LocalDate.of(2017, 1, 1).toEpochDay();
+				long randomDay = minDay + random.nextInt(maxDay - minDay);
+
+				LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+
+				writer.printf("db.clients.insert({name:" + "\"%s" + "\",birthdate:" + "\"%s" + "\"});%n",
+						names[new Random().nextInt(names.length)], randomBirthDate);
+
+			}
+
+			// BETWEEN 1980 - 1990
+			for (int i = 1; i <= 2000; i++) {
+				String names[] = { "Jay", "Jim", "Yom", "Axel", "Billy", "Charlie", "Jax", "Gina", "Paul", "Ringo",
+						"Ally", "Nicky", "Cam", "Ari", "Trudie", "Cal", "Carl", "Lady", "Lauren", "Ichabod", "Arthur",
+						"Ashley", "Drake", "Kim", "Julio", "Lorraine", "Floyd", "Janet", "Lydia", "Charles", "Pedro",
+						"Bradley" };
+				Random random = new Random();
+				int minDay = (int) LocalDate.of(1981, 1, 1).toEpochDay();
+				int maxDay = (int) LocalDate.of(1990, 1, 1).toEpochDay();
+				long randomDay = minDay + random.nextInt(maxDay - minDay);
+
+				LocalDate randomBirthDate = LocalDate.ofEpochDay(randomDay);
+
+				writer.printf("db.clients.insert({name:" + "\"%s" + "\",birthdate:" + "\"%s" + "\"});%n",
+						names[new Random().nextInt(names.length)], randomBirthDate);
+
+			}
+			writer.close();
+
 		} catch (Exception e) {
-    }
-   	 
-    
-	 
+		}
 
+	}
 }
-}
-
